bc1aa0e83aea7e7bde2f265cc637f2eae07eb97e
beburkart <beburkart@solyaine.home>
2017-06-12 19:10:42 +0200

    Unity-Scene-Grundaufbau

---
+++ vr_prototype/Assets/Scripts/camera_movement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camera_movement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.UpArrow)) {
+			float yPos = transform.position.y + 1f;
+			transform.position = new Vector3 (0, yPos, 0);
+		}
+		if (Input.GetKeyDown (KeyCode.DownArrow)) {
+			float yPos = transform.position.y - 1f;
+			transform.position = new Vector3 (0, yPos, 0);
+		}
+	}
+
+	public void CameraPositionUp () {
+			float yPos = transform.position.y + 1f;
+			transform.position = new Vector3(0, yPos, 0);
+		}
+
+	public void CameraPositionDown () {
+			float yPos = transform.position.y - 1f;
+			transform.position = new Vector3(0, yPos, 0);
+		}
+}
